16868e5d9bdce0b3552dd791e9e3278c1295d9f9
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2022-04-17 10:36:38 +0700

    В BranchBD добавлено свойство headOrganization

--- src/database/branch-bd.ts
+++ src/database/branch-bd.ts
@@ -1,5 +1,6 @@
 type Branch = {
   id: number;
+  headOrganization: number;
   address: string;
   executive: string;
   telephone: string;
@@ -8,18 +9,21 @@ type Branch = {
 const branchBD: Branch[] = [
   {
     id: 1,
+    headOrganization: 1,
     address: 'г. Астрахань, ул. Новая, д. 116',
     telephone: '87778884444 ',
     executive: 'Белов Василий Михайлович',
   },
   {
     id: 2,
+    headOrganization: 2,
     address: 'г. Чебоксары, ул. Шоссейная, д. 87',
     telephone: '89754975432 ',
     executive: 'Михеева Оксана федоровна',
   },
   {
     id: 3,
+    headOrganization: 3,
     address: 'г. Новосибирск, ул. Николая Буды, д. 73',
     telephone: '87779246897',
     executive: 'Конев Максим Адреевич',
