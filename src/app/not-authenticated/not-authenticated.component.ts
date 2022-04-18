2e77808f22055e26ec161f3c8cc324b2b69eddde
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2022-04-18 16:48:26 +0700

    Добавлено выравнивание для странцы not-authenticated

--- src/app/not-authenticated/not-authenticated.component.ts
+++ src/app/not-authenticated/not-authenticated.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-not-authenticated',
   templateUrl: './not-authenticated.component.html',
-  styleUrls: ['./not-authenticated.component.scss']
+  styleUrls: ['./not-authenticated.component.scss'],
+  host: { class: 'not-authenticated' },
+  encapsulation: ViewEncapsulation.None
 })
 export class NotAuthenticatedComponent implements OnInit {
 
